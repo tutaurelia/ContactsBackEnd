f9947e298441f304a381764b38fcced40bf26458
tutaurelia <tutaurelia@outlook.com>
2015-05-21 21:25:48 +0200

    SQL Scripts ContactsBackend

---
+++ CreateContactsTable.sql
@@ -0,0 +1,17 @@
+﻿GO
+USE [ContactsDB];
+
+GO
+CREATE TABLE [dbo].[Contacts] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (50)  NULL,
+    [LastName]  VARCHAR (50)  NULL,
+    [Address]   VARCHAR (MAX) NULL,
+    [ZipCode]   INT           NULL,
+    [City]      VARCHAR (50)  NULL,
+    [Telephone] VARCHAR (20)  NULL,
+    [Email]     VARCHAR (MAX) NULL,
+    [BirthDate] DATETIME      NULL,
+    CONSTRAINT [pk_ContactPErsonId] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Insert10RowsIntoContactsTable.sql
@@ -0,0 +1,12 @@
+USE [ContactsDB]
+
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('wim', 'abbas',  'Zwijnaardsesteenweg 744', 2540, 'HOVE', '03 455 19 62', 'wim.abbas@tutaurelia.net', '1985-10-15')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('angelov', 'abbasi',  'Zwijnaardsesteenweg 557', 1330, 'RIXENSART', '02 652 22 86', 'angelov.abbasi@tutaurelia.net', '1979-3-2')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('hans', 'abdi sharmarke',  'Zwijnaardse Steenweg 5 / 7', 1200, 'BRUXELLES', '02 770 09 34', 'hans.abdi.sharmarke@tutaurelia.net', '1966-6-22')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('fatou', 'abdulhameed jam',  'Zwarte Vijverstraat 71', 8400, 'OOSTENDE', '059 70 52 60', 'fatou.abdulhameed.jam@tutaurelia.net', '1967-5-15')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('bernard', 'abdullahi',  'Zur Domäne 7', 1970, 'WEZEMBEEK-OPPEM', '02 731 23 57', 'bernard.abdullahi@tutaurelia.net', '1964-11-15')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('bergmans', 'abi char',  'Zuidlaan 64', 1000, 'BRUSSEL', '02 644 00 00', 'bergmans.abi.char@tutaurelia.net', '1979-11-24')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('gerry', 'abied',  'Zuiderlaan 1-3', 1180, 'BRUXELLES', '02 376 56 50', 'gerry.abied@tutaurelia.net', '1997-12-6')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('gerrit', 'abon escalona',  'Zuiderakker 4', 9090, 'MELLE', '09 225 59 49', 'gerrit.abon.escalona@tutaurelia.net', '1992-4-20')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('meiying', 'aboulakil',  'Zuiderakker 4', 1070, 'BRUXELLES', '02 521 63 15', 'meiying.aboulakil@tutaurelia.net', '1974-6-15')
+INSERT INTO [dbo].[Contacts] (FirstName, LastName, [Address], ZipCode, City, Telephone, Email, BirthDate) VALUES ('yi', 'abraham',  'Zoutelaan 81', 1180, 'BRUXELLES', '02 375 84 08', 'yi.abraham@tutaurelia.net', '1988-4-28')
